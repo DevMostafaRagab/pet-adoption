582ba8a205dcd0e67f17ab0a364d6b19fd1b2049
Mostafa Ragab <DevMostafaRagab@gmail.com>
2026-06-20 16:43:33 +0300

    Completed <template> tag section

--- pets.js
+++ pets.js
@@ -19,10 +19,20 @@ async function showPetsData() {
     //1// "clone" will be used to injected dynamic data into html tags
     const clone = template.content.cloneNode(true);
 
-
     //2// display pets data
     clone.querySelector("h3").textContent = pet.name;
-    clone.querySelector(".pet-description").textContent = pet.description;
+    clone.querySelector(".pet-card-description").textContent = pet.description;
+    // call formatAge function to calculates pet's age (in years)
+    clone.querySelector(".pet-card-age").textContent = formatAge(pet.birthYear);
+
+    // if there no photo (images failing to load), display a fallback image
+    if (!pet.photo) pet.photo = "./images/fallback.jpg";
+
+    // display pets photo
+    clone.querySelector(".pet-card-photo img").src = pet.photo;
+    clone.querySelector(".pet-card-photo img").alt = `A ${pet.species} named ${pet.name}.`;
+
+
 
     //3//=> THIS "wrapper" MUST BE LAST LINE 
     wrapper.appendChild(clone);
@@ -33,3 +43,13 @@ async function showPetsData() {
 
 // call function
 showPetsData();
+
+// calculates pet's age (in years) from birth year and returns it as a human-readable age string
+function formatAge(birthYear) {
+  // Get the current year from the system clock
+  const currentYear = new Date().getFullYear();
+  const age = currentYear - birthYear;
+  if (age == 1) return "1 year old";
+  if (age == 0) return "Less than a year old";
+  return `${age} years old`;
+}
